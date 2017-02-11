5fe4bed3413a6d8aba9ff0de506f0df15f500806
Ted Shaffer <ted@brightsign.biz>
2017-02-11 07:58:14 -0800

    set zone div styles dynamically based on zone properties.

--- src/components/sign.js
+++ src/components/sign.js
@@ -8,31 +8,7 @@ export default class Sign extends Component {
 
   render () {
 
-    const styleTop = {
-      position: 'absolute',
-      left: '0px',
-      top: '0px',
-      width: '960px',
-      height: '540px'
-    };
-
-    const styleBottom = {
-      position: 'absolute',
-      left: '960px',
-      top: '540px',
-      width: '960px',
-      height: '540px'
-    };
-
     const zones = this.props.sign.zones;
-
-    zones[0].className = 'zoneTop';
-    zones[0].style = styleTop;
-
-    zones[1].className = 'zoneBottom';
-    zones[1].style = styleBottom;
-
-    // className={zone.className}
 
     return (
       <div>
@@ -40,13 +16,19 @@ export default class Sign extends Component {
           zones.map( (zone, index) =>
             <div
               key={index}
-              style={zone.style}
+              style={
+              {
+                position: 'absolute',
+                left: zone.position.x,
+                top: zone.position.y,
+                width: zone.position.width,
+                height: zone.position.height
+              }
+              }
             >
               <Zone
                 platform={this.props.platform}
                 playlist={zone.playlist}
-                x={Number(zone.position.x)}
-                y={Number(zone.position.y)}
                 width={Number(zone.position.width)}
                 height={Number(zone.position.height)}
               />
