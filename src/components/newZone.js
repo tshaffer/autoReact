e5b67281499241f9d9e3b5b2ba5777d72eef66dc
Ted Shaffer <ted@brightsign.biz>
2017-02-26 09:00:46 -0800

    ongoing progress

--- src/components/newZone.js
+++ src/components/newZone.js
@@ -11,7 +11,7 @@ import {
   // DataFeedTypeName,
   // dmGetDataFeedById,
   // dmGetMediaStateStateById,
-  // ContentItemTypeName,
+  ContentItemTypeName,
   // dmOpenSign,
   // dmAddHtmlSite,
   // dmCreateMediaContentItem,
@@ -24,24 +24,25 @@ import {
   // VideoMode,
   // PlayerModel,
   // dmAddZone,
-  // ZoneType,
+  ZoneType,
   // dmGetSignMetaData,
   // dmGetZonesForSign,
   // dmGetZoneById,
+  dmMediaState,
   dmGetMediaStateById,
   dmGetZoneSimplePlaylist,
-  // dmGetEventIdsForMediaState,
+  dmGetEventIdsForMediaState,
   // dmGetTransitionIdsForEvent,
   // dmGetTransitionById,
-  // MediaType,
+  MediaType,
   // VideoModeName,
   // TransitionTypeName,
-  // EventTypeName,
-  // dmGetEventById,
+  EventTypeName,
+  dmGetEventById,
   // GraphicsZOrderTypeName,
   // TouchCursorDisplayModeTypeName,
   // UdpAddressTypeName,
-  // ZoneTypeCompactName,
+  ZoneTypeCompactName,
   // ViewModeTypeName,
   // ImageModeTypeName,
   // AudioOutputSelectionTypeName,
@@ -73,13 +74,29 @@ export default class NewZone extends Component {
     super(props);
 
     this.state = {
-      assetIndex: 0,
+      zoneType: '',
       mediaStateIds: [],
       mediaStates: [],
+      mediaStateIndex: 0,
     };
   }
 
   componentWillMount() {
+
+    let zoneType = ZoneTypeCompactName(this.props.zone.type);
+    this.setState( { zoneType });
+
+    switch (zoneType) {
+      case 'Ticker': {
+        return;
+      }
+      case 'VideoOrImages': {
+        break;
+      }
+      default: {
+        debugger;
+      }
+    }
     const mediaStateIds = dmGetZoneSimplePlaylist(this.props.bsdm, { id: this.props.zone.id });
     let mediaStates = mediaStateIds.map( mediaStateId => {
       return dmGetMediaStateById(this.props.bsdm, { id : mediaStateId })
@@ -99,8 +116,88 @@ export default class NewZone extends Component {
       );
     }
 
+    if (this.state.zoneType === 'Ticker') {
+      return (
+        <div>Ticker support pending</div>
+      );
+    }
+
     console.log(this.state.mediaStateIds);
     console.log(this.state.mediaStates);
+
+    debugger;
+
+    const currentMediaState = this.state.mediaStates[this.state.mediaStateIndex];
+
+    const mediaStateContainer = currentMediaState.container;
+    const mediaStateContainerType = mediaStateContainer.type;
+
+    const mediaStateContentItem = currentMediaState.contentItem;
+    const mediaStateContentItemType = mediaStateContentItem.type;
+    const contentItemType = ContentItemTypeName(mediaStateContentItemType).toLowerCase();
+    switch (contentItemType) {
+      case 'media': {
+
+        const name = mediaStateContentItem.name;
+        const mediaObject = mediaStateContentItem.media;
+
+        const assetId = mediaObject.assetId;
+        const mediaType = mediaObject.mediaType;
+
+        // switch (mediaType) {
+        //   case MediaType.Image: {
+        //     break;
+        //   }
+        //   case MediaType.Video: {
+        //     break;
+        //   }
+        //   default: {
+        //     debugger;
+        //   }
+        // }
+        break;
+      }
+      case 'html': {
+        debugger;
+        break;
+      }
+      case 'mrssfeed': {
+        debugger;
+        break;
+      }
+      case 'datafeed': {
+        debugger;
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+
+    let eventIds = dmGetEventIdsForMediaState(this.props.bsdm, { id : currentMediaState.id });
+    if (eventIds.length !== 1) {
+      debugger;
+    }
+
+    let event = dmGetEventById(this.props.bsdm, { id : eventIds[0] });
+    if (!event) {
+      debugger;
+    }
+
+    const eventName = EventTypeName(event.type);
+    switch(eventName) {
+      case 'Timer': {
+        const timeout = event.data.interval;
+        break;
+      }
+      case 'MediaEnd': {
+        debugger;
+        break;
+      }
+      default: {
+        debugger;
+      }
+    }
 
     return (
       <div>poo</div>
