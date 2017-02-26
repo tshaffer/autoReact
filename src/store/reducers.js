e93af7882302cde9d52a15e8f5b38516b64a7d8b
Ted Shaffer <ted@brightsign.biz>
2017-02-26 14:54:49 -0800

    remove unused sign object.

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,12 +2,10 @@
 
 import {combineReducers} from 'redux';
 import { bsDmReducer } from '@brightsign/bsdatamodel';
-// import SignReducer from './sign';
 import presentationsReducer from './presentations';
 
 const rootReducer = combineReducers({
   bsdm: bsDmReducer,
-  // sign: SignReducer,
   presentations: presentationsReducer,
 });
 
