dd87ee61e0de5aa70e1e6ba18c612cb1da5fbd3a
Ted Shaffer <ted@brightsign.biz>
2017-03-11 07:03:38 -0800

    progress on upload to device

--- src/index.js
+++ src/index.js
@@ -53,369 +53,300 @@ ReactDOM.render(
 
 // LWS handlers
 
+// FIXME - platform
 // const targetFolder = '/Users/tedshaffer/Desktop/baconLWSTest';
-// app.use(bodyParser.urlencoded({ extended: false }));
-//
-// app.get('/SpecifyCardSizeLimits', (_, res) => {
-//   // const limitStorageSpace = req.query.limitStorageSpace;
-//   console.log('SpecifyCardSizeLimits invoked');
-//   res.send('SpecifyCardSizeLimits');
-// });
+const targetFolder = 'storage/sd';
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/SpecifyCardSizeLimits', (_, res) => {
+  // const limitStorageSpace = req.query.limitStorageSpace;
+  console.log('SpecifyCardSizeLimits invoked');
+  res.send('SpecifyCardSizeLimits');
+});
+
+// FIXME - platform
 const upload = multer({ dest: 'storage/sd/uploads/' });
 // const upload = multer({ dest: 'uploads/' });
 
-// app.post('/PrepareForTransfer', upload.array('files', 1), function (req, res) {
-//
-//   console.log(req.body);
-//   console.log(req.files);
-//
-//   const file = req.files[0];
-//
-//   // let { destination, encoding, fieldname, filename, mimetype, originalname, path, size } = file;
-//   let path = file.path;
-//
-//   // read xml file
-//   const filesToPublishXML = fs.readFileSync(path);
-//
-//   // parseFileToPublish(req.body['nameParam1']).then( (filesToPublish : Array<Object>) => {
-//   //
-//   //   // let filesToCopy = freeSpaceOnDrive(filesToPublish);
-//   //   freeSpaceOnDrive(filesToPublish).then( (filesToCopy) => {
-//   //
-//   //     debugger;
-//   //     // let filesToCopy = {};
-//   //     filesToCopy.file = [];
-//   //
-//   //     filesToPublish.forEach( (fileToPublish) => {
-//   //       let fileToCopy = {};
-//   //       const filePath = fileToPublish.filePath[0];
-//   //       const fileName = fileToPublish.fileName[0];
-//   //       const hashValue = fileToPublish.hashValue[0];
-//   //       const fileSize = fileToPublish.fileSize[0];
-//   //
-//   //       fileToCopy = {
-//   //         filePath,
-//   //         fileName,
-//   //         hashValue,
-//   //         fileSize
-//   //       };
-//   //       filesToCopy.file.push(fileToCopy);
-//   //     });
-//   //
-//   //
-//   //     // convert to xml
-//   //     const filesToPublishXml = js2xmlparser('filesToCopy', filesToCopy);
-//   //
-//   //     res.send(filesToPublishXml);
-//   //     res.end("File is uploaded");
-//   //
-//   //   }).catch( (err) => {
-//   //     console.log(err);
-//   //     debugger;
-//   //   });
-//   // });
-//
-//   // parseFileToPublish(req.body['nameParam1']).then( (filesToPublish) => {
-//
-//
-//   parseFileToPublish(filesToPublishXML).then( (filesToPublish : Array<Object>) => {
-//
-//     freeSpaceOnDrive(filesToPublish).then( (missingFiles : Object) => {
-//
-//       let requiredFiles = {};
-//       requiredFiles.file = [];
-//
-//       for (let fileProperty in missingFiles) {
-//
-//         if (missingFiles.hasOwnProperty(fileProperty)) {
-//
-//           const missingFile = missingFiles[fileProperty];
-//           let requiredFile = {
-//             filePath: missingFile.filePath,
-//             fileName: missingFile.fileName,
-//             hashValue: missingFile.hashValue,
-//             fileSize: missingFile.fileSize
-//           };
-//           requiredFiles.file.push(requiredFile);
-//         }
-//       }
-//
-//       // convert to xml
-//       const filesToPublishXml = js2xmlparser('filesToCopy', requiredFiles);
-//
-//       res.send(filesToPublishXml);
-//       res.end('File is uploaded');
-//     });
-//
-//     // let filesToCopy = {};
-//     // filesToCopy.file = [];
-//     //
-//     // filesToPublish.forEach( (fileToPublish) => {
-//     //   let fileToCopy = {};
-//     //   const filePath = fileToPublish.filePath[0];
-//     //   const fileName = fileToPublish.fileName[0];
-//     //   const hashValue = fileToPublish.hashValue[0];
-//     //   const fileSize = fileToPublish.fileSize[0];
-//     //
-//     //   fileToCopy = {
-//     //     filePath,
-//     //     fileName,
-//     //     hashValue,
-//     //     fileSize
-//     //   };
-//     //   filesToCopy.file.push(fileToCopy);
-//     // });
-//     //
-//     // // convert to xml
-//     // const filesToPublishXml = js2xmlparser('filesToCopy', filesToCopy);
-//     //
-//     // res.send(filesToPublishXml);
-//     // res.end("File is uploaded");
-//
-//   }).catch( (err) => {
-//     console.log(err);
-//     debugger;
-//   });
-// });
-//
-//
-// function getFiles(dir: string) {
-//
-//   return new Promise( (resolve, reject) => {
-//
-//     readDir(dir, (err, files) => {
-//
-//       if (err) {
-//         reject(err);
-//         return;
-//       }
-//
-//       resolve(files);
-//     });
-//   });
-// }
-//
-//
-// function freeSpaceOnDrive(filesToPublish : Array<Object>) {
-//
-//   return new Promise( (resolve, reject) => {
-//
-//     // files that need to be copied by BrightAuthor
-//     let filesToCopy = {};
-//
-//     // files that can be deleted to make room for more content
-//     let deletionCandidates = {};
-//
-//     // create list of files already on the card in the pool folder
-//     const poolPath = path.join(targetFolder, 'pool');
-//     getFiles(poolPath).then( (arrayOfPoolFiles : Array<string>) => {
-//
-//       let listOfPoolFiles = {};
-//       arrayOfPoolFiles.forEach( (filePath) => {
-//         const fileName = path.basename(filePath);
-//         listOfPoolFiles[fileName] = filePath;
-//         deletionCandidates[fileName] = filePath;
-//       });
-//
-//       // create the list of files that need to be copied.
-//       // this is the list of files in filesToPublish that are not in listOfPoolFiles
-//       let totalSpaceRequired = 0;
-//       filesToPublish.forEach( (fileToPublish : Object) => {
-//         const fullFileName = fileToPublish.fullFileName;
-//         if (!deletionCandidates[fullFileName]) {
-//           let fileItem = {};
-//           fileItem.fileName = fileToPublish.fileName[0];
-//           fileItem.filePath = fileToPublish.filePath[0];
-//           fileItem.hashValue = fileToPublish.hashValue[0];
-//           fileItem.fileSize = fileToPublish.fileSize[0];
-//
-//           filesToCopy[fullFileName] = fileItem;
-//
-//           totalSpaceRequired += Number(fileItem.fileSize);
-//         }
-//       });
-//
-// /*
-//  https://www.npmjs.com/package/nodejs-disks
-//  ' determine if additional space is required
-//   du = CreateObject("roStorageInfo", "./")
-//   freeInMegabytes! = du.GetFreeInMegabytes()
-//   totalFreeSpace! = freeInMegabytes! * 1048576
-//
-//   if m.limitStorageSpace.....
-//
-//
-//   deleteUnneededFiles = false
-//   if totalFreeSpace! < totalSpaceRequired! then
-//     deleteUnneededFiles = true
-//   endif
-//   if m.limitStorageSpace then
-//     if totalSizeOfPoolAfterCopy! > budgetedMaximumPoolSize then
-//       deleteUnneededFiles = true
-//     endif
-//   endif
-//
-//  */
-//
-//       resolve(filesToCopy);
-//     }).catch ( (err) => {
-//       reject(err);
-//     });
-//   });
-// }
-//
-//
-//
-//
-//
-//
-//
-//
-// // TODO - uploads limited to 50 MBytes
+app.post('/PrepareForTransfer', upload.array('files', 1), function (req, res) {
+
+  console.log(req.body);
+  console.log(req.files);
+
+  const file = req.files[0];
+
+  // let { destination, encoding, fieldname, filename, mimetype, originalname, path, size } = file;
+  let path = file.path;
+
+  // read xml file
+  const filesToPublishXML = fs.readFileSync(path);
+
+  parseFileToPublish(filesToPublishXML).then( (filesToPublish : Array<Object>) => {
+
+    freeSpaceOnDrive(filesToPublish).then( (missingFiles : Object) => {
+
+      let requiredFiles = {};
+      requiredFiles.file = [];
+
+      for (let fileProperty in missingFiles) {
+
+        if (missingFiles.hasOwnProperty(fileProperty)) {
+
+          const missingFile = missingFiles[fileProperty];
+          let requiredFile = {
+            filePath: missingFile.filePath,
+            fileName: missingFile.fileName,
+            hashValue: missingFile.hashValue,
+            fileSize: missingFile.fileSize
+          };
+          requiredFiles.file.push(requiredFile);
+        }
+      }
+
+      // convert to xml
+      const filesToPublishXml = js2xmlparser('filesToCopy', requiredFiles);
+
+      res.send(filesToPublishXml);
+      res.end('File is uploaded');
+    });
+  }).catch( (err) => {
+    console.log(err);
+    debugger;
+  });
+});
+
+
+function getFiles(dir: string) {
+
+  return new Promise( (resolve, reject) => {
+
+    readDir(dir, (err, files) => {
+
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve(files);
+    });
+  });
+}
+
+
+function freeSpaceOnDrive(filesToPublish : Array<Object>) {
+
+  return new Promise( (resolve, reject) => {
+
+    // files that need to be copied by BrightAuthor
+    let filesToCopy = {};
+
+    // files that can be deleted to make room for more content
+    let deletionCandidates = {};
+
+    // create list of files already on the card in the pool folder
+    const poolPath = path.join(targetFolder, 'pool');
+    getFiles(poolPath).then( (arrayOfPoolFiles : Array<string>) => {
+
+      let listOfPoolFiles = {};
+      arrayOfPoolFiles.forEach( (filePath) => {
+        const fileName = path.basename(filePath);
+        listOfPoolFiles[fileName] = filePath;
+        deletionCandidates[fileName] = filePath;
+      });
+
+      // create the list of files that need to be copied.
+      // this is the list of files in filesToPublish that are not in listOfPoolFiles
+      let totalSpaceRequired = 0;
+      filesToPublish.forEach( (fileToPublish : Object) => {
+        const fullFileName = fileToPublish.fullFileName;
+        if (!deletionCandidates[fullFileName]) {
+          let fileItem = {};
+          fileItem.fileName = fileToPublish.fileName[0];
+          fileItem.filePath = fileToPublish.filePath[0];
+          fileItem.hashValue = fileToPublish.hashValue[0];
+          fileItem.fileSize = fileToPublish.fileSize[0];
+
+          filesToCopy[fullFileName] = fileItem;
+
+          totalSpaceRequired += Number(fileItem.fileSize);
+        }
+      });
+
+/*
+ https://www.npmjs.com/package/nodejs-disks
+ ' determine if additional space is required
+  du = CreateObject("roStorageInfo", "./")
+  freeInMegabytes! = du.GetFreeInMegabytes()
+  totalFreeSpace! = freeInMegabytes! * 1048576
+
+  if m.limitStorageSpace.....
+
+
+  deleteUnneededFiles = false
+  if totalFreeSpace! < totalSpaceRequired! then
+    deleteUnneededFiles = true
+  endif
+  if m.limitStorageSpace then
+    if totalSizeOfPoolAfterCopy! > budgetedMaximumPoolSize then
+      deleteUnneededFiles = true
+    endif
+  endif
+
+ */
+
+      resolve(filesToCopy);
+    }).catch ( (err) => {
+      reject(err);
+    });
+  });
+}
+
+// TODO - uploads limited to 50 MBytes
+// FIXME - platform
 // const uploadLarge = multer({ dest: 'uploads/', limits: { fieldSize : 50000000 } });
-// app.post('/UploadFile', uploadLarge.array('files', 1), function (req, res) {
-//
-//   console.log(req.body);
-//   console.log(req.files);
-//
-//   const file = req.files[0];
-//
-//   // fields available in req.files from multer
-//   // let { destination, encoding, fieldname, filename, mimetype, originalname, path, size } = file;
-//   const uploadedFilePath = file.path;
-//
-//   const destinationFilePath = req.headers['destination-filename'];
-//   const targetFilePath = getTargetPathFromDestinationFilePath(destinationFilePath, targetFolder);
-//   // const fileName = req.headers['friendly-filename'];
-//   const filePath = path.join(targetFolder, targetFilePath);
-//
-//   // move file from path to filePath
-//   var source = fs.createReadStream(uploadedFilePath);
-//   var dest = fs.createWriteStream(filePath);
-//
-//   console.log('copying file from: ', uploadedFilePath, ' to: ', filePath);
-//   source.pipe(dest);
-//   source.on('end', () => {
-//     console.log('copy complete');
-//     // TODO - delete source?
-//   });
-//
-//   source.on('error', function(err) {
-//     console.log('copy failed: ', err);
-//   });
-//
-//   res.send('ok');
-// });
-//
-// // function str2ab(str) {
-// //   console.log('create arrayBuffer with size: ', str.length);
-// //   var buf = new ArrayBuffer(str.length);
-// //   var bufView = new Uint8Array(buf);
-// //   for (var i=0, strLen=str.length; i<strLen; i++) {
-// //     bufView[i] = str.charCodeAt(i);
-// //   }
-// //   return buf;
-// // }
-//
-// export function writeFile(filePath: string, data: string) {
-//   return new Promise( (resolve, reject) => {
-//     fs.writeFile( filePath, data, (err) => {
-//       if (err) {
-//         reject(err);
-//       }
-//       else {
-//         resolve();
-//       }
-//     });
-//   });
+const uploadLarge = multer({ dest: 'storage/sd/uploads/', limits: { fieldSize : 50000000 } });
+app.post('/UploadFile', uploadLarge.array('files', 1), function (req, res) {
+
+  console.log(req.body);
+  console.log(req.files);
+
+  const file = req.files[0];
+
+  // fields available in req.files from multer
+  // let { destination, encoding, fieldname, filename, mimetype, originalname, path, size } = file;
+  const uploadedFilePath = file.path;
+
+  const destinationFilePath = req.headers['destination-filename'];
+  const targetFilePath = getTargetPathFromDestinationFilePath(destinationFilePath, targetFolder);
+  // const fileName = req.headers['friendly-filename'];
+  const filePath = path.join(targetFolder, targetFilePath);
+
+  // move file from path to filePath
+  var source = fs.createReadStream(uploadedFilePath);
+  var dest = fs.createWriteStream(filePath);
+
+  console.log('copying file from: ', uploadedFilePath, ' to: ', filePath);
+  source.pipe(dest);
+  source.on('end', () => {
+    console.log('copy complete');
+    // TODO - delete source?
+  });
+
+  source.on('error', function(err) {
+    console.log('copy failed: ', err);
+  });
+
+  res.send('ok');
+});
+
+// function str2ab(str) {
+//   console.log('create arrayBuffer with size: ', str.length);
+//   var buf = new ArrayBuffer(str.length);
+//   var bufView = new Uint8Array(buf);
+//   for (var i=0, strLen=str.length; i<strLen; i++) {
+//     bufView[i] = str.charCodeAt(i);
+//   }
+//   return buf;
 // }
-//
-// app.post('/UploadSyncSpec', upload.array('files', 1), function (req, res) {
-//
-//   debugger;
-//
-//   console.log(req.body);
-//   console.log(req.files);
-//
-//   const file = req.files[0];
-//
-//   // let { destination, encoding, fieldname, filename, mimetype, originalname, path, size } = file;
-//
-//   const newSyncSpec = fs.readFileSync(file.path);
-//
-//   // convert to xml
-//   const newSyncSpecXml = js2xmlparser('sync', newSyncSpec);
-//
-//   // get targetPath
-//   // TODO - where / how to save this. autoxml.brs saves in tmp
-//   const fileName = 'new-sync.xml';
-//   let filePath = path.join(targetFolder, fileName);
-//
-//   fs.writeFileSync(filePath, newSyncSpecXml);
-//
-//   res.send('ok');
-// });
-//
+
+export function writeFile(filePath: string, data: string) {
+  return new Promise( (resolve, reject) => {
+    fs.writeFile( filePath, data, (err) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve();
+      }
+    });
+  });
+}
+
+app.post('/UploadSyncSpec', upload.array('files', 1), function (req, res) {
+
+  debugger;
+
+  console.log(req.body);
+  console.log(req.files);
+
+  const file = req.files[0];
+
+  // let { destination, encoding, fieldname, filename, mimetype, originalname, path, size } = file;
+
+  const newSyncSpec = fs.readFileSync(file.path);
+
+  // convert to xml
+  const newSyncSpecXml = js2xmlparser('sync', newSyncSpec);
+
+  // get targetPath
+  // TODO - where / how to save this. autoxml.brs saves in tmp
+  const fileName = 'new-sync.xml';
+  let filePath = path.join(targetFolder, fileName);
+
+  fs.writeFileSync(filePath, newSyncSpecXml);
+
+  res.send('ok');
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port);
-//
-//
-// // function toBuffer(ab) {
-// //   let buf = new Buffer(ab.byteLength);
-// //   let view = new Uint8Array(ab);
-// //   for (let i = 0; i < buf.length; ++i) {
-// //     buf[i] = view[i];
-// //   }
-// //   return buf;
-// // }
-//
-// function parseFileToPublish(filesToPublishXML : string) {
-//
-//   return new Promise( (resolve, reject) => {
-//
-//     let parser = new xml2js.Parser();
-//     try {
-//       parser.parseString(filesToPublishXML, (err, filesToPublish) => {
-//         if (err) {
-//           console.log(err);
-//           debugger;
-//         }
-//         console.log(filesToPublish.files.file);
-//         resolve(filesToPublish.files.file);
-//       });
-//     }
-//     catch (e) {
-//       console.log(e);
-//       reject();
-//     }
-//   });
+
+
+// function toBuffer(ab) {
+//   let buf = new Buffer(ab.byteLength);
+//   let view = new Uint8Array(ab);
+//   for (let i = 0; i < buf.length; ++i) {
+//     buf[i] = view[i];
+//   }
+//   return buf;
 // }
-//
-// function getTargetPathFromDestinationFilePath(destinationFileName : string, rootDir : string) {
-//
-//   const dir0 = destinationFileName.charAt(destinationFileName.length - 2);
-//   const dir1 = destinationFileName.charAt(destinationFileName.length - 1);
-//
-//   const parts = destinationFileName.split("/");
-//
-//   // create directories if they do not exist
-//   // TODO - don't use synchronous calls
-//   let dirName = path.join(rootDir, parts[0]);
-//   createDir(dirName);
-//   dirName = path.join(dirName, dir0);
-//   createDir(dirName);
-//   dirName = path.join(dirName, dir1);
-//   createDir(dirName);
-//
-//   const fileName = parts[1];
-//
-//   return path.join('pool/', dir0, dir1, fileName);
-// }
-//
-// function createDir(dir : string) {
-//   console.log('Do not mkdir in case running on a BrightSign');
-//   // if (!fs.existsSync(dir)){
-//   //   fs.mkdirSync(dir);
-//   // }
-// }
+
+function parseFileToPublish(filesToPublishXML : string) {
+
+  return new Promise( (resolve, reject) => {
+
+    let parser = new xml2js.Parser();
+    try {
+      parser.parseString(filesToPublishXML, (err, filesToPublish) => {
+        if (err) {
+          console.log(err);
+          debugger;
+        }
+        console.log(filesToPublish.files.file);
+        resolve(filesToPublish.files.file);
+      });
+    }
+    catch (e) {
+      console.log(e);
+      reject();
+    }
+  });
+}
+
+function getTargetPathFromDestinationFilePath(destinationFileName : string, rootDir : string) {
+
+  const dir0 = destinationFileName.charAt(destinationFileName.length - 2);
+  const dir1 = destinationFileName.charAt(destinationFileName.length - 1);
+
+  const parts = destinationFileName.split("/");
+
+  // create directories if they do not exist
+  // TODO - don't use synchronous calls
+  let dirName = path.join(rootDir, parts[0]);
+  createDir(dirName);
+  dirName = path.join(dirName, dir0);
+  createDir(dirName);
+  dirName = path.join(dirName, dir1);
+  createDir(dirName);
+
+  const fileName = parts[1];
+
+  return path.join('pool/', dir0, dir1, fileName);
+}
+
+function createDir(dir : string) {
+  console.log('Do not mkdir in case running on a BrightSign');
+  // if (!fs.existsSync(dir)){
+  //   fs.mkdirSync(dir);
+  // }
+}
