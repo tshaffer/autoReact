615b9e9c92e9c988695b71a187a7a0c87731df25
Ted Shaffer <ted@brightsign.biz>
2017-05-06 09:33:18 -0700

    diagnostics to catch when a feed is updated.

--- src/entities/liveDataFeed.js
+++ src/entities/liveDataFeed.js
@@ -27,6 +27,17 @@ export class ARLiveDataFeed {
 
     const rssChannel = rssData.rss.channel[0];
     const rssItemSpecs = rssChannel.item;
+
+    // check for change in feed
+    if (rssItemSpecs.length !== this.rssItems.length) {
+      console.log('parseSimpleRSSFeed - length changed');
+    }
+    rssItemSpecs.forEach( (rssItemSpec, index ) => {
+      if (this.rssItems.length > index && rssItemSpec.title[0] !== this.rssItems[index].title) {
+        console.log('parseSimpleRSSFeed - content changed');
+      }
+    });
+
     this.rssItems = rssItemSpecs.map(rssItemSpec => {
       return {
         description: rssItemSpec.description[0],
